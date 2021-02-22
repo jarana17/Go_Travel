0700f3f4d3b882ce0ba1eb94664eb897c4890667
jarana <jeferssonarana17@gmail.com>
2021-02-22 18:18:22 -0500

    Faltante del javascript

---
+++ javascript/script.js
@@ -0,0 +1,74 @@
+
+function llanos(){
+    swal("Oriente", "Destinos: San martin, Acacias, Villavicencio y Puerto lópez\n"+
+    "Dias: 4 dias\n"+
+    "Noches: 3 noches\n" +
+    "Comida: Desayuno incluido tipo americano ( huevos, tocineta, arroz y chocolate)\n"+
+    "Hospedaje: Hoteles 3 estrellas\n"+
+    "Transporte: Tiquete desde bogotá, cali o medellín, movilización entre las ciudades con guía turístico\n"+
+    "Tour: Recorrido en los parques, tour por las distintos sitios turísticos y snack en atardecer llanero en el ombligo de colombia \n"+
+    
+    " Por Persona: $550.000");
+    
+}
+
+function sanandres(){
+    swal("San Andrés", "Destinos: San Andrés e islas\n" +
+    "Dias: 4 dias\n" +
+    "Noches: 3 noches\n" +
+    "Comida: Desayuno incluido tipo americano ( huevos, tocineta, arroz y chocolate)\n" +
+    "Hospedaje: Hotel 4 estrellas\n" +
+    "Transporte: Tiquete desde bogotá, cali o medellín, movilización entre la ciudad con guía turístico\n" +
+    "Tour: Recorrido en los parques, tour por las distintos sitios turísticos y boat party barra libre\n" + 
+    
+    "Por Persona: $700.000");
+    
+}
+
+function ejecafetero(){
+    swal("Eje Cafetero", "Destinos: Manizales, Pereira y armenia\n" +
+    "Dias: 5 dias\n" +
+    "Noches: 4 noches\n" +
+    "Comida: Desayuno incluido tipo americano ( huevos, tocineta, arroz y chocolate)\n" +
+    "Hospedaje: Hoteles 3 estrellas\n" +
+    "Transporte: Tiquete desde Bogotá o Medellín, movilización entre las ciudades con guía turístico\n" +
+    "Tour: Recorrido en los parques, tour por las distintas ciudades y una noche en chiva rumbera\n" +
+    
+    "Por Persona: $650.000");
+    
+}
+function costacaribe(){
+    swal("Costa Caribe", "Destinos: Santa marta, Barranquilla y Cartagena\n" +
+    "Dias: 6 dias\n" +
+    "Noches: 5 noches\n" +
+    "Comida: Desayuno incluido tipo americano ( huevos, tocineta, arroz y chocolate)\n" +
+    "Hospedaje: Hoteles 3 estrellas frente al mar\n" +
+    "Transporte: Tiquete desde bogotá, cali o medellín, movilización entre las ciudades con guía turístico\n" +
+    "Tour: Tour por las 3 ciudades en los mejores sitios turísticos y playas\n" +
+    
+    "Por Persona: $900.000");
+    
+}
+function antioquia(){
+    swal("Antioquia", "Destinos: Medellín, piedra peñol, guatapé y  rionegro\n" +
+   " Dias: 4 dias\n" +
+    "Noches: 3 noches\n" +
+    "Comida: Desayuno incluido tipo americano ( huevos, tocineta, arroz y chocolate)\n" +
+    "Hospedaje: Hoteles 3 estrellas\n" +
+    "Transporte: Tiquete desde bogotá, cali o cartagena, movilización entre las ciudades con guía turístico\n" +
+    "Tour: Recorrido en los parques, tour por las distintos sitios turísticos y cena en guatapé\n" +
+    
+    "Por Persona: $550.000");
+    
+}
+function amazonia(){
+    swal("Amazonia", "Destinos: Leticia y parques naturales\n" +
+    "Dias: 3 dias\n" +
+    "Noches: 2 noches\n" +
+    "Comida: Desayuno incluido tipo americano ( huevos, tocineta, arroz y chocolate)\n" +
+    "Hospedaje: Hotel 4 estrellas\n" +
+    "Transporte: Tiquete desde bogotá, cali o medellín, movilización entre la ciudad con guía turístico\n" +
+    "Tour: Recorrido en los parques, tour por las distintos sitios turísticos y boat party barra libre \n" +
+    
+    "Por Persona: $500.000");
+}
